85f211ba87acb80c48429e7c95576669f5d72add
earwickerh <60644170+earwickerh@users.noreply.github.com>
2020-08-09 03:08:59 -0400

    5c9aebf rverton

--- webanalyze.go
+++ webanalyze.go
@@ -205,7 +205,7 @@ func process(job *Job, appDefs *AppsDefinition) ([]Match, []string, error) {
 	} else {
 		resp, err := fetchHost(job.URL)
 		if err != nil {
-			return nil, links, fmt.Sprintf("error retrieving: %v\n", err)
+			return nil, links, fmt.Errorf("Failed to retrieve: %v", err)
 		}
 
 		defer resp.Body.Close()
