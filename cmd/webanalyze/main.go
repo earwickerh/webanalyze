1a5d55c40393f629246430602e0de302e2a4e2c2
earwickerh <60644170+earwickerh@users.noreply.github.com>
2020-08-10 04:39:19 -0400

    revert "catch error from csvWrite"

--- cmd/webanalyze/main.go
+++ cmd/webanalyze/main.go
@@ -164,7 +164,7 @@ func output(result webanalyze.Result, wa *webanalyze.WebAnalyzer, outWriter *csv
 
 	case "csv":
 		for _, m := range result.Matches {
-			err := outWriter.Write(
+			outWriter.Write(
 				[]string{
 					result.Host,
 					strings.Join(m.CatNames, ","),
@@ -172,7 +172,6 @@ func output(result webanalyze.Result, wa *webanalyze.WebAnalyzer, outWriter *csv
 					m.Version,
 				},
 			)
-			if err != nil { log.Printf("error writing csv: %v\n", err) }
 		}
 		outWriter.Flush()
 	case "json":
